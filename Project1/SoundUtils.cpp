f3354356c64ee09c0d9b4efe4c76f28404d41ba2
Kihz17 <mitchdawdy@gmail.com>
2021-09-28 04:39:19 -0400

    Merge SoundObject with SoundUtils

--- Project1/SoundUtils.cpp
+++ Project1/SoundUtils.cpp
@@ -23,3 +23,83 @@ FMOD::Sound* CreateSound(FMOD::System* system, std::string audioFile, std::strin
 	ss.flush();
 	return sound;
 }
+
+FMOD::Sound* GetSound(std::string soundName)
+{
+	std::map<std::string, FMOD::Sound*>::iterator it = sounds.find(soundName);
+	if (it != sounds.end()) // We found the sound
+	{
+		return it->second;
+	}
+	else // No sound was found
+	{
+		return NULL;
+	}
+}
+
+
+void PlaySound(FMOD::Channel* channel, std::string soundName, bool isPaused)
+{
+	FMOD::Sound* sound = GetSound(soundName);
+	if (sound != NULL)
+	{
+		_system->playSound(sound, 0, isPaused, &channel);
+	}
+}
+
+void SetPaused(FMOD::Channel* channel, bool isPaused)
+{
+	_result = channel->setPaused(isPaused);
+	if (_result != FMOD_OK)
+	{
+		fprintf(stderr, "Unable to pause channel.");
+	}
+}
+
+void SetFrequency(FMOD::Channel* channel, float frequency)
+{
+	_result = channel->setFrequency(frequency);
+	if (_result != FMOD_OK)
+	{
+		fprintf(stderr, "Unable to set channel frequency.");
+	}
+}
+
+void SetVolume(FMOD::Channel* channel, float volume)
+{
+	_result = channel->setVolume(volume);
+	if (_result != FMOD_OK)
+	{
+		fprintf(stderr, "Unable to set channel volume.");
+	}
+}
+
+void SetPan(FMOD::Channel* channel, float pan)
+{
+	_result = channel->setPan(pan);
+	if (_result != FMOD_OK)
+	{
+		fprintf(stderr, "Unable to set channel pan.");
+	}
+}
+
+void SetSpeed(std::string soundName, float speed)
+{
+	FMOD::Sound* sound = GetSound(soundName);
+	if (sound == NULL)
+	{
+		fprintf(stderr, "Failed to set sound speed.");
+		return;
+	}
+
+	SetSpeed(sound, speed);
+}
+
+void SetSpeed(FMOD::Sound* sound, float speed)
+{
+	_result = sound->setMusicSpeed(speed);
+	if (_result != FMOD_OK)
+	{
+		fprintf(stderr, "Unable to set sound speed.");
+	}
+}
--- Project1/SoundUtils.h
+++ Project1/SoundUtils.h
@@ -10,3 +10,27 @@ extern FMOD_RESULT _result;
 extern std::map<std::string, FMOD::Sound*> sounds; // Holds all loaded sounds
 
 FMOD::Sound* CreateSound(FMOD::System* system, std::string audioFile, std::string soundName);
+
+// Gets a sound from our sounds map. Returns NULL if the sound is not found
+FMOD::Sound* GetSound(std::string soundName);
+
+// Play a sound for this object
+void PlaySound(FMOD::Channel* channel, std::string soundName, bool isPaused);
+
+// Pause the audio currenty playing
+void SetPause(FMOD::Channel* channel, bool isPaused);
+
+// Set the channel's frequency
+void SetFrequency(FMOD::Channel* channel, float frequency);
+
+// Set the channel's volume
+void SetVolume(FMOD::Channel* channel, float volume);
+
+// Set the channel's volume
+void SetPan(FMOD::Channel* channel, float pan);
+
+// Set the channel's volume
+void SetSpeed(std::string soundName, float speed);
+
+// Set a sound's playback speed
+void SetSpeed(FMOD::Sound* sound, float speed);
